249c299100330baef6329468e60e8228239fb4c4
Iain Smith <iainsmith@users.noreply.github.com>
2024-08-26 20:07:50 +0100

    Update ctrl keys in wezterm

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -7,4 +7,7 @@ config.color_scheme = 'Tokyo Night'
 config.font =
   wezterm.font('JetBrains Mono', { weight = 'Bold', italic = false })
 
+config.send_composed_key_when_left_alt_is_pressed = true
+config.send_composed_key_when_right_alt_is_pressed = true
+
 return config
